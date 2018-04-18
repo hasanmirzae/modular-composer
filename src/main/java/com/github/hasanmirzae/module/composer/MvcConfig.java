0cfa8715a3ab1aee47e983dd1fd5ee46dc318b23
Hassan Mirzaee <hassan_mirzaee@epam.com>
2018-04-18 13:51:24 +0200

    dev spring boot app

---
+++ src/main/java/com/github/hasanmirzae/module/composer/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.github.hasanmirzae.module.composer;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebMvc
+//@EnableAutoConfiguration
+public class MvcConfig implements WebMvcConfigurer {
+
+}
